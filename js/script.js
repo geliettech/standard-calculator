e68c6700ac12de3e1e7c60e30d573e5f88769e36
julietoge <julietogechi27@gmail.com>
2023-12-08 20:19:05 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+// When the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
+  // DOM elements
   let display = document.getElementById("display");
   let clearAll = document.getElementById("clearDisplay");
   let operator = document.querySelectorAll(".operator");
@@ -7,37 +9,45 @@ document.addEventListener("DOMContentLoaded", function () {
   let percentage = document.getElementById("percentage");
   let calculate = document.getElementById("calculateResult");
 
+  // Adding click event listeners to operator and digit buttons
   operator.forEach(dataValue);
   digit.forEach(dataValue);
 
+  // Adding click event listeners to other buttons
   percentage.addEventListener("click", percent);
   backSpace.addEventListener("click", Back);
   clearAll.addEventListener("click", clearDisplay);
   calculate.addEventListener("click", calculateResult);
 });
 
+// Function to handle click events for operator and digit buttons
 const dataValue = (button) => {
   button.addEventListener("click", function () {
     appendDisplay(button.getAttribute("data-value"));
   });
 };
 
+// Function to append values to the display
 const appendDisplay = (value) => {
   display.value += value;
 };
 
+// Function to calculate percentage
 const percent = () => {
   display.value = eval(display.value) / 100;
 };
 
+// Function to handle backspace
 const Back = () => {
   display.value = display.value.slice(0, -1);
 };
 
+// Function to clear the display
 const clearDisplay = () => {
   display.value = "";
 };
 
+// Function to calculate the result
 const calculateResult = () => {
   try {
     display.value = eval(display.value);
