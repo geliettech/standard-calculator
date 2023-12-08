a5eb5fc4af9aa68e16e7509d4f67e48d2b350368
julietoge <julietogechi27@gmail.com>
2023-12-08 16:37:27 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   let display = document.getElementById("display");
   let clearAll = document.getElementById("clearDisplay");
-  let backSpace = document.getElementById("backSpace");
-  let calculate = document.getElementById("calculateResult");
-  let percentage = document.getElementById("percentage");
-  let digit = document.querySelectorAll(".digit");
   let operator = document.querySelectorAll(".operator");
+  let backSpace = document.getElementById("backSpace");
+  let digit = document.querySelectorAll(".digit");
+  let percentage = document.getElementById("percentage");
+  let calculate = document.getElementById("calculateResult");
 
-  clearAll.addEventListener("click", clearDisplay);
-  backSpace.addEventListener("click", Back);
-  calculate.addEventListener("click", calculateResult);
-  percentage.addEventListener("click", percent);
-
-  digit.forEach(dataValue);
   operator.forEach(dataValue);
+  digit.forEach(dataValue);
+
+  percentage.addEventListener("click", percent);
+  backSpace.addEventListener("click", Back);
+  clearAll.addEventListener("click", clearDisplay);
+  calculate.addEventListener("click", calculateResult);
 });
+
+const dataValue = (button) => {
+  button.addEventListener("click", function () {
+    appendDisplay(button.getAttribute("data-value"));
+  });
+};
 
 const appendDisplay = (value) => {
   display.value += value;
@@ -24,18 +30,12 @@ const percent = () => {
   display.value = eval(display.value) / 100;
 };
 
-const clearDisplay = () => {
-  display.value = "";
-};
-
 const Back = () => {
   display.value = display.value.slice(0, -1);
 };
 
-const dataValue = (button) => {
-  button.addEventListener("click", function () {
-    appendDisplay(button.getAttribute("data-value"));
-  });
+const clearDisplay = () => {
+  display.value = "";
 };
 
 const calculateResult = () => {
